cb611bcafbb57407dc288065e010c8ed2ce34f3b
pranjaldas <pranjaldas525@gmail.com>
2022-03-12 19:23:46 +0530

    Versioning is done

--- src/main/java/click/pranjalonline/blogs/config/SecurityConfig.java
+++ src/main/java/click/pranjalonline/blogs/config/SecurityConfig.java
@@ -48,8 +48,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET,"/api/**").permitAll()
-                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/v1/**").permitAll()
+                .antMatchers("/api/v1/auth/**").permitAll()
                 .anyRequest()
                 .authenticated();
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
